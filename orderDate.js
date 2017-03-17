dfcdd38c15d068053f0163eb3297140c743af4d7
Henrique <rtrans.web@gmail.com>
2017-03-17 17:06:55 +0100

    Don't use parameter for data processing

--- orderDate.js
+++ orderDate.js
@@ -1,35 +1,37 @@
 function orderDate(date) {
 
-  var timeStp = [];
+            var timeStp = [];
+            
+            var dateConvert = [];
 
-  for (var i = 0; i < date.length; i++) {
-    
-    date[i] = date[i].split('/');
-    
-    date[i] = date[i][1] + '/' + date[i][0] + '/' + date[i][2];
-    
-    timeStp[i] = Date.parse(date[i]);
-    
-  }
+            for (var i = 0; i < date.length; i++) {
 
-  timeStp.sort();
+                dateConvert[i] = date[i].split('/');
 
-  var ascendingDate = [];
+                dateConvert[i] = dateConvert[i][1] + '/' + dateConvert[i][0] + '/' + dateConvert[i][2];
 
-  for (var j = 0; j < timeStp.length; j++) {
-    
-    ascendingDate[j] = new Date(timeStp[j]);
-    
-    ascendingDate[j] = ascendingDate[j].toLocaleDateString();
-    
-    ascendingDate[j] = ascendingDate[j].split("/");
-    
-    ascendingDate[j][2] = ascendingDate[j][2].slice(2);
-    
-    ascendingDate[j] = ascendingDate[j][0] + '/' + ascendingDate[j][1] + '/' + ascendingDate[j][2];
-    
-  }
+                timeStp[i] = Date.parse(dateConvert[i]);
 
-  return ascendingDate;
+            }
 
-}
+            timeStp.sort();
+
+            var ascendingDate = [];
+
+            for (var j = 0; j < timeStp.length; j++) {
+
+                ascendingDate[j] = new Date(timeStp[j]);
+
+                ascendingDate[j] = ascendingDate[j].toLocaleDateString();
+
+                ascendingDate[j] = ascendingDate[j].split("/");
+
+                ascendingDate[j][2] = ascendingDate[j][2].slice(2);
+
+                ascendingDate[j] = ascendingDate[j][0] + '/' + ascendingDate[j][1] + '/' + ascendingDate[j][2];
+
+            }
+
+            return ascendingDate;
+
+        }
